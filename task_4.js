ea5d801b86664adb82144e13577da9108f5a8db6
Ju17-art <madam.yulis@mail.ru>
2026-04-02 16:56:37 +0300

    add tsk4

--- task_4.js
+++ task_4.js
@@ -1,22 +1,14 @@
-// Создаем Promise
 const randomNumberPromise = new Promise((resolve, reject) => {
-  // Задержка в 3 секунды с помощью setTimeout
   setTimeout(() => {
-    // Генерируем случайное целое число от 1 до 100
     const randomNumber = Math.floor(Math.random() * 100) + 1;
-
-    // Проверяем четность числа
     if (randomNumber % 2 === 0) {
-      // Четное число - успешное выполнение
       resolve(randomNumber);
     } else {
-      // Нечетное число - ошибка
       reject(randomNumber);
     }
   }, 3000);
 });
 
-// Обрабатываем результат Promise
 randomNumberPromise
   .then((number) => {
     console.log(`Завершено успешно. Сгенерированное число — ${number}`);
